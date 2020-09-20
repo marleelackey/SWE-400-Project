8b2738c2ffef205489b50a8f651fe053fa9abc86
Dan Holmgren <dh7548@cs.ship.edu>
2020-09-20 19:03:56 -0400

    added all methods needed, used strings instead of dto for time being

---
+++ SingleTableInheritance/src/TableDataGateway.java
@@ -0,0 +1,94 @@
+import java.util.ArrayList;
+
+import Datasource.DatabaseException;
+import Datasource.DatabaseManager;
+
+import java.sql.*;
+
+/**
+ * 
+ * @author Dan Holmgren
+ * @author Joshua Kellogg
+ *
+ */
+public class TableDataGateway {
+//	private int ID,
+//				type,
+//				atomicNumber,
+//				soluteA,
+//				soluteB,
+//				dissolvedBy;
+//	private String name;
+//	private double atomicMass;
+//	private ArrayList<Integer> madeOf = new ArrayList<Integer>();
+//	private	ArrayList<Integer> dissolves = new ArrayList<Integer>();
+	
+	private static TableDataGateway instance;
+	private TableDataGateway() {
+		
+	}
+	
+	/**
+	 * @author Dan Holmgren
+	 * @return an instance of the TDG
+	 */
+	public static TableDataGateway getSingleton() {
+		if(instance == null) {
+			instance = new TableDataGateway();
+		} 
+		return instance;
+	}
+	
+	private ArrayList<String> getAllBases() throws DatabaseException, SQLException{
+		ArrayList<String> data = new ArrayList<String>();
+		ResultSet rs;
+		
+		try {
+			Connection cn = DatabaseManager.getSingleton().getConnection();
+			rs = cn.createStatement().executeQuery("SELECT * FROM Chemical WHERE type = 1");
+			while(rs.next()) {
+				data.add(rs.getString("Name"));
+			}
+		} catch (SQLException | DatabaseException e){
+			
+		}
+		return data;
+	}
+	
+	private ArrayList<String> getElementsInRange(double lower, double upper) {
+		ArrayList<String> data = new ArrayList<String>();
+		ResultSet rs;
+		
+		try {
+			Connection cn = DatabaseManager.getSingleton().getConnection();
+			rs = cn.createStatement().executeQuery("SELECT * FROM Chemical WHERE atomicMass > " + lower
+					+ "AND WHERE atomicMass < " + upper);
+			while(rs.next()) {
+				data.add(rs.getString("Name"));
+			}
+			
+		} catch(SQLException | DatabaseException e) {
+			
+		}
+		
+		return data;
+	}
+	
+	private ArrayList<String> getCompoundsByElement(int ID) {
+		ArrayList<String> data = new ArrayList<String>();
+		ResultSet rs;
+		
+		try {
+			Connection cn = DatabaseManager.getSingleton().getConnection();
+			rs = cn.createStatement().executeQuery("SELECT * FROM CompoundMadeOfElement WHERE ElementID = " + ID);
+			while(rs.next()) {
+				data.add(rs.getString("Name"));
+			}
+			
+		} catch(SQLException | DatabaseException e) {
+			
+		}
+		
+		return data;
+	}
+}
