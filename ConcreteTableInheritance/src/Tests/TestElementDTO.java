d8fc453c3030864934cad3b529a54779f91da56a
Dan Holmgren <dh7548@cs.ship.edu>
2020-09-23 12:15:12 -0400

    changed TestElementDTO to a junit test class

--- ConcreteTableInheritance/src/Tests/TestElementDTO.java
+++ ConcreteTableInheritance/src/Tests/TestElementDTO.java
@@ -1,5 +1,14 @@
 package Tests;
 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
 public class TestElementDTO {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
 
 }
