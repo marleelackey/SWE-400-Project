ce4f25c54b88fdb5e3e70cfc8767fb3e68e48826
Madeline Cardinale <mc6672@cs.ship.edu>
2020-09-23 00:39:07 -0400

    my brain is a puddle

--- SingleTableInheritance/src/Classes/ChemicalDTO.java
+++ SingleTableInheritance/src/Classes/ChemicalDTO.java
@@ -1,3 +1,5 @@
+package Classes;
+
 /**
  * @authors Madeline & Adam
  *
---
+++ SingleTableInheritance/src/Tests/TestChemicalDTO.java
@@ -0,0 +1,23 @@
+package Tests;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import Classes.ChemicalDTO;
+
+public class TestChemicalDTO {
+
+	@Test
+	public void testConstructorAndGetters() {
+		ChemicalDTO dto = new ChemicalDTO(1, 2, "Boron", 5, 21.3, 7, 9, 12);
+
+		assertEquals(dto.getID(), 1);
+		assertEquals(dto.getType(), 2);
+		assertEquals(dto.getName(), "Boron");
+		assertEquals(dto.getAtomicNumber(), 5);
+		assertEquals(dto.getAtomicMass(), 21.3);
+		assertEquals(dto.getDissolvedBy(), 7);
+		assertEquals(dto.getSoluteA(), 9);
+		assertEquals(dto.getSoluteB(), 12);
+	}
+
+}
