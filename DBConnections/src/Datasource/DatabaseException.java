591be7e7551a730b92e966d375c93f282afdadec
Bev Cardinale <mc6672@cs.ship.edu>
2020-09-23 22:08:44 -0400

    dan get your life together

---
+++ DBConnections/src/Datasource/DatabaseException.java
@@ -0,0 +1,73 @@
+package Datasource;
+
+import java.sql.SQLException;
+
+/**
+ * @author Merlin
+ */
+public class DatabaseException extends Exception
+{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private String simpleDescription;
+
+	private Exception rootCause;
+
+	/**
+	 * 
+	 * @param msg the message associated with this exception
+	 */
+	public DatabaseException(String msg)
+	{
+		simpleDescription = msg;
+	}
+
+	/**
+	 * @param msg description of complication
+	 * @param e exception being thrown
+	 */
+	public DatabaseException(String msg, Exception e)
+	{
+		simpleDescription = msg;
+		rootCause = e;
+	}
+
+	/**
+	 * 
+	 * @return the original exception, if any, that occurred
+	 */
+	public Exception getRootCause()
+	{
+		return rootCause;
+	}
+
+	/**
+	 * @return simple Description
+	 */
+	public String getSimpleDescription()
+	{
+		return simpleDescription;
+	}
+
+	/**
+	 * @see java.lang.Throwable#toString()
+	 */
+	public String toString()
+	{
+		return simpleDescription + ": " + rootCause;
+	}
+	
+	/**
+	 * @author mad
+	 * @param e
+	 */
+	public static void detectError(Exception e)
+	{	
+		DatabaseException d = new DatabaseException("yeet", e);
+		System.out.println(d.toString());
+	}
+
+}
