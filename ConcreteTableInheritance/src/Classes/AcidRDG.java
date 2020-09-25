7460e25ec9575bac6fd22ddbf7bf8b3f4b51cb46
Madeline Cardinale <mc6672@cs.ship.edu>
2020-09-24 20:16:19 -0400

    shelving the mental breakdown for now

--- ConcreteTableInheritance/src/Classes/AcidRDG.java
+++ ConcreteTableInheritance/src/Classes/AcidRDG.java
@@ -1,5 +1,10 @@
 package Classes;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import Datasource.DatabaseException;
+import Datasource.DatabaseManager;
+
 /**
  * A Row Data Gateway for the Acid table
  * 
@@ -23,6 +28,44 @@ public class AcidRDG {
 		acidID = ID;
 		acidName = name;
 		acidSolute = solute;
+	}
+
+	/**
+	 * Method to query the database for an Acid selected by its ID
+	 * 
+	 * @param ID the ID of the Acid we want to select
+	 * @return an AcidRDG for the Acid we select
+	 */
+	public AcidRDG findByID(int ID) {
+		Connection c;
+		AcidRDG result = null;
+		try {
+			c = DatabaseManager.getSingleton().getConnection();
+			ResultSet rs = c.createStatement().executeQuery("SELECT * FROM ACID WHERE ID = " + ID);
+			result = new AcidRDG(rs.getInt(0), rs.getString(1), rs.getInt(2));
+		} catch (Exception e) {
+			DatabaseException.detectError(e);
+		}
+		return result;
+	}
+
+	/**
+	 * Method to query the database for an Acid selected by its name
+	 * 
+	 * @param name the name of the Acid we want to select
+	 * @return an AcidRDG for the Acid we select
+	 */
+	public AcidRDG findByName(String name) {
+		Connection c;
+		AcidRDG result = null;
+		try {
+			c = DatabaseManager.getSingleton().getConnection();
+			ResultSet rs = c.createStatement().executeQuery("SELECT * FROM ACID WHERE NAME = " + name);
+			result = new AcidRDG(rs.getInt(0), rs.getString(1), rs.getInt(2));
+		} catch (Exception e) {
+			DatabaseException.detectError(e);
+		}
+		return result;
 	}
 
 	public int getAcidID() {
