d376f5b1b1db8c47d722a4bf10c3ad27d2c766df
Marlee Lackey <ml1548@cs.ship.edu>
2020-09-25 14:10:15 -0400

    Still needs work but have the barebones done

--- ConcreteTableInheritance/src/Classes/CompoundRDG.java
+++ ConcreteTableInheritance/src/Classes/CompoundRDG.java
@@ -1,5 +1,10 @@
 package Classes;
 
+import java.sql.*;
+
+import Datasource.DatabaseException;
+import Datasource.DatabaseManager;
+
 /**
  * 
  * @author Marlee Lackey
@@ -10,6 +15,7 @@ public class CompoundRDG {
 	
 	private int compoundID;
 	private String compoundName;
+	Connection connection;
 
 	/**
 	 * Constructor for CompoundRDG
@@ -19,6 +25,13 @@ public class CompoundRDG {
 	public CompoundRDG (int id, String name) {
 		compoundID = id;
 		compoundName = name;
+		
+		try {
+			connection = DatabaseManager.getSingleton().getConnection();
+		} catch (DatabaseException e) {
+			DatabaseException.detectError(e);
+		}	
+
 	}
 
 	// getters 
@@ -38,6 +51,32 @@ public class CompoundRDG {
 	public void setCompoundName(String compoundName) {
 		this.compoundName = compoundName;
 	}
+	
+	// find bys
+	
+	// finders
+		public CompoundDTO findByIDConcrete(int ID) {
+			
+			// make more secure later when we know how to setInt() for Strings...
+			String query = "SELECT * FROM Compound WHERE Compound.compoundID = " + ID;
+			ResultSet rs = null;
+			CompoundDTO comp = null;
+			try {
+				Statement statement = connection.createStatement();
+				rs = statement.executeQuery(query);
+				if(rs.next()) {
+					comp = new CompoundDTO(ID, compoundName);
+				 }
+			}
+			catch (SQLException e) {
+				DatabaseException.detectError(e);
+			}
+
+			return comp;
+		}
+	
+	
+	// update
 	
 
 }
