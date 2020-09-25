b1493dbcd27a2fc4adfaae0831e7431672413d0a
Marlee Lackey <ml1548@cs.ship.edu>
2020-09-25 12:33:40 -0400

    Constructor made

---
+++ ConcreteTableInheritance/src/Classes/CompoundRDG.java
@@ -0,0 +1,24 @@
+package Classes;
+
+/**
+ * 
+ * @author Marlee Lackey
+ * @author Taryn Whitman
+ *
+ */
+public class CompoundRDG {
+	
+	private int compoundID;
+	private String compoundName;
+	
+	/**
+	 * Constructor for CompoundRDG
+	 * @param id Passes in the ID for CompoundRDG
+	 * @param name Passes in the Name for CompoundRDG
+	 */
+	public CompoundRDG (int id, String name) {
+		compoundID = id;
+		compoundName = name;
+	}
+
+}
