43660ec98f29373a137ba299f38734b2c1c57f8d
Marlee Lackey <ml1548@cs.ship.edu>
2020-09-27 23:06:13 -0400

    class created, with getters and simple constructor

---
+++ ConcreteTableInheritance/src/Classes/CompoundMadeOfElement.java
@@ -0,0 +1,31 @@
+package Classes;
+
+/**
+ * 
+ * @author Marlee Lackey
+ * @author Taryn Whitman
+ *
+ */
+public class CompoundMadeOfElement {
+
+	private int compoundID;
+	private int elementID;
+	
+	public CompoundMadeOfElement(int compoundID, int elementID) {
+		this.compoundID = compoundID;
+		this.elementID = elementID;
+	}
+	
+	// getters
+	public int getElementID() {
+		return elementID;
+	}
+	
+	public int getCompoundID() {
+		return compoundID;
+	}
+	
+	
+	
+	
+}
