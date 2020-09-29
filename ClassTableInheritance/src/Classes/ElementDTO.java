f2f7281407e6fa8392904750121467ee97411afe
Dan Holmgren <dh7548@engr.ship.edu>
2020-09-28 22:51:25 -0400

    finsihed elementDTO for class, need to add runAllTests later

--- ClassTableInheritance/src/Classes/ElementDTO.java
+++ ClassTableInheritance/src/Classes/ElementDTO.java
@@ -1,5 +1,41 @@
 package Classes;
 
+/**
+ * 
+ * @author Dan Holmgren
+ * @author Josh Kellogg
+ *
+ */
 public class ElementDTO {
+	private int ID,
+				atomicNumber;
+	private double atomicMass;
+	
+	public ElementDTO(int ID, int atomicNumber, double atomicMass) {
+		this.ID = ID;
+		this.atomicNumber = atomicNumber;
+		this.atomicMass = atomicMass;
+	}
 
+	
+	//Getters and Setters
+	public int getID() {
+		return ID;
+	}
+
+	public int getAtomicNumber() {
+		return atomicNumber;
+	}
+
+	public void setAtomicNumber(int atomicNumber) {
+		this.atomicNumber = atomicNumber;
+	}
+
+	public double getAtomicMass() {
+		return atomicMass;
+	}
+
+	public void setAtomicMass(double atomicMass) {
+		this.atomicMass = atomicMass;
+	}
 }
--- ClassTableInheritance/src/Tests/TestElementDTO.java
+++ ClassTableInheritance/src/Tests/TestElementDTO.java
@@ -4,11 +4,21 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import Classes.ElementDTO;
+
 public class TestElementDTO {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testConstructor() {
+		ElementDTO e = new ElementDTO(1, 2, 3.0);
+		assertEquals(e.getID(), 1);
+		assertEquals(e.getAtomicNumber(), 2);
+		assertEquals(e.getAtomicMass(), 3.0, 0.01);
+		
+		e.setAtomicMass(65136513.0);
+		assertEquals(e.getAtomicMass(), 65136513.0, 0.01);
+		e.setAtomicNumber(532);
+		assertEquals(e.getAtomicNumber(), 532);
 	}
 
 }
