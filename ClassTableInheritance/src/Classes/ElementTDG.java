230d1e6cac3253f867ff7147d47811ae1a482d63
Dan Holmgren <dh7548@engr.ship.edu>
2020-09-28 23:06:12 -0400

    finished elementTDG, need to add runAllTests later

---
+++ ClassTableInheritance/src/Classes/ElementTDG.java
@@ -0,0 +1,42 @@
+package Classes;
+
+import java.sql.*;
+import java.util.ArrayList;
+
+import Datasource.DatabaseException;
+import Datasource.DatabaseManager;
+
+public class ElementTDG {
+
+	private static ElementTDG instance;
+	
+	private ElementTDG() {
+		
+	}
+	
+	public static ElementTDG getInstance() {
+		if(instance == null) {
+			instance = new ElementTDG();
+		}
+		return instance;
+	}
+	
+	public ArrayList<ElementDTO> getElementInRange(double lower, double upper){
+		ArrayList<ElementDTO> data = new ArrayList<ElementDTO>();
+		ResultSet rs;
+		try {
+			DatabaseManager db = DatabaseManager.getSingleton();
+			db.setPatternNumber(3);
+			Connection cn = db.getConnection();
+			rs = cn.createStatement().executeQuery("SELECT * FROM Element WHERE elementAtomicMass > " + lower + " AND elementAtomicMass < " + upper);
+			while(rs.next()) {
+				data.add(new ElementDTO(rs.getInt("elementID"), rs.getInt("elementAtomicNumber"), rs.getDouble("elementAtomicMass")));
+			}
+			
+		} catch(Exception e) {
+			DatabaseException.detectError(e);
+		}
+		
+		return data;
+	}
+}
---
+++ ClassTableInheritance/src/Tests/TestElementTDG.java
@@ -0,0 +1,35 @@
+package Tests;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+import Classes.ElementDTO;
+import Classes.ElementTDG;
+
+public class TestElementTDG {
+
+	@Test
+	public void testConstructor() {
+		ElementTDG e = ElementTDG.getInstance();
+		assertNotEquals(e, null);
+	}
+	
+	@Test
+	public void testGetElementsInRange() {
+		ElementTDG c = ElementTDG.getInstance();
+		ArrayList<ElementDTO> result = new ArrayList<ElementDTO>();
+		result = c.getElementInRange(0, 100);
+		//Test to make sure it gets the correct number of dtos
+		assertEquals(4, result.size());
+		//Test to make sure all dto atomic masses are in range
+		for(int i = 0; i < result.size(); i++) {
+			if((result.get(i).getAtomicMass() < 1) || result.get(i).getAtomicMass() > 100) {
+				fail("Atomic mass not in range");
+			}
+		}
+	}
+
+}
