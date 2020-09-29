69fe9c81c9b976c1c1603d35037c10434abb0dfc
Marlee Lackey <ml1548@cs.ship.edu>
2020-09-29 17:42:37 -0400

    Constructor made

---
+++ ClassTableInheritance/src/Classes/CompoundMadeOfElement.java
@@ -0,0 +1,24 @@
+package Classes;
+
+/**
+ * 
+ * @author Marlee Lackey
+ * @author Taryn Whitman
+ *
+ */
+public class CompoundMadeOfElement {
+
+	private int compoundID;
+	private int elementID;
+	
+	/**
+	 * Constructor
+	 * @param compID ID of compound
+	 * @param elemID ID of element
+	 */
+	public CompoundMadeOfElement(int compID, int elemID) {
+		this.compoundID = compID;
+		this.elementID = elemID;
+	}
+	
+}
