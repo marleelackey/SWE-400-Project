75e0621ad5536fc6a37bb7d309dd5c88f075b483
Marlee Lackey <ml1548@cs.ship.edu>
2020-09-29 17:47:35 -0400

    need to add getters then test will be done

---
+++ ClassTableInheritance/src/Tests/TestCompoundMadeOfElement.java
@@ -0,0 +1,23 @@
+package Tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import Classes.CompoundMadeOfElement;
+
+public class TestCompoundMadeOfElement {
+
+	@Test
+	public static void testConstructor() {
+		CompoundMadeOfElement comp = new CompoundMadeOfElement(1,2);
+		assertEquals(comp.getCompoundID(), 1);
+		assertEquals(comp.getElementID(), 2);
+	}
+
+	
+	@Test
+	public static void runAllTheTests() {
+		testConstructor();
+	}
+}
