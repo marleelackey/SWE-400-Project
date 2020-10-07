5a43bc27201bc88167d6438143c0a40e42c63beb
Dan Holmgren <dh7548@cs.ship.edu>
2020-10-07 11:16:50 -0400

    Added all the panels

--- PresentationDomain/src/GUI.java
+++ PresentationDomain/src/GUI.java
@@ -1,8 +1,36 @@
+import javax.swing.*;
+
 /**
  * This here be the gui
  * @author Joshua Kellogg
  *
  */
-public class GUI {
-
+public class GUI 
+{
+	JFrame mainFrame = new JFrame("SWE Rocks!");
+	
+	JPanel elementMainPanel = new JPanel();
+	JPanel listOfElementsPanel = new JPanel();
+	JPanel elementControlPanel = new JPanel();
+	
+	JPanel metalMainPanel = new JPanel();
+	JPanel listOfMetalsPanel = new JPanel();
+	JPanel metalControlPanel = new JPanel();
+	
+	JPanel compoundMainPanel = new JPanel();
+	JPanel listOfCompoundPanel = new JPanel();
+	JPanel compoundControlPanel = new JPanel();
+	
+	JPanel acidMainPanel = new JPanel();
+	JPanel listOfAcidsPanel = new JPanel();
+	JPanel acidControlPanel = new JPanel();
+	
+	JPanel baseMainPanel = new JPanel();
+	JPanel listOfBasesPanel = new JPanel();
+	JPanel baseControlPanel = new JPanel();
+	
+	JPanel chemicalMainPanel = new JPanel();
+	JPanel listOfChemicalsPanel = new JPanel();
+	JPanel chemicalControlPanel = new JPanel();
+	
 }
