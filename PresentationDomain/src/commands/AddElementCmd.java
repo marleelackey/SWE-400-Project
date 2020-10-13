11a3dc341bbaf9d3caae242db0727750d10090be
Bev Cardinale <mc6672@cs.ship.edu>
2020-10-12 23:44:47 -0400

    add element cmd is done (i think)

--- PresentationDomain/src/commands/AddElementCmd.java
+++ PresentationDomain/src/commands/AddElementCmd.java
@@ -4,6 +4,7 @@
 package commands;
 
 import mappers.ElementMapper;
+import mappers.MetalMapper;
 
 /**
  * 
@@ -20,35 +21,50 @@ public class AddElementCmd implements CommandInterface {
 	private double elementAtomicMass;
 	private boolean isMetal;
 	private int acidID;
+	private double elementMoles;
+	private double elementMolesOfAcidToDissolve;
 
 	/**
 	 * Constructor
 	 * 
-	 * @param id    the ID of the element
-	 * @param name  the name of the element
-	 * @param num   the atomic number of the element
-	 * @param mass  the atomic mass of the element
-	 * @param metal whether the element is a metal or not
-	 * @param a_id  the acid that dissolves the element if it's a metal
+	 * @param id            the ID of the element
+	 * @param name          the name of the element
+	 * @param num           the atomic number of the element
+	 * @param mass          the atomic mass of the element
+	 * @param metal         whether the element is a metal or not
+	 * @param a_id          the acid that dissolves the element if it's a metal
+	 * @param mol           the number of moles of the element we have in inventory
+	 * @param molToDissolve the number of moles of an acid it takes to dissolve a
+	 *                      mole of the element
 	 */
-	public AddElementCmd(int id, String name, int num, double mass, boolean metal, int a_id) {
+	public AddElementCmd(int id, String name, int num, double mass, boolean metal, int a_id, double mol,
+			double molToDissolve) {
 		elementID = id;
 		elementName = name;
 		elementAtomicNumber = num;
 		elementAtomicMass = mass;
 		isMetal = metal;
 		acidID = a_id;
+		elementMoles = mol;
+		elementMolesOfAcidToDissolve = molToDissolve;
 	}
 
 	/**
-	 * Execute the AddElementCmd by creating a mapper to handle the insertion
+	 * Execute the AddElementCmd by creating a mapper and receiving a domain object
 	 */
 	@Override
 	public void execute() {
-		ElementMapper em = new ElementMapper(1, "hello", 5, 4.4, 6.6);
-		//ElementDomainObject edo = em.createElement(elementID, elementName, elementAtomicNumber, elementAtomicMass,
-			//	isMetal, acidID);
-		//edo.persist();
+		if (isMetal()) {
+			MetalMapper mm = new MetalMapper(elementID, elementName, elementAtomicNumber, elementAtomicMass, acidID,
+					elementMoles, elementMolesOfAcidToDissolve);
+			MetalDomainObject mdo = mm.createMetal();
+			mdo.persist();
+		} else {
+			ElementMapper em = new ElementMapper(elementID, elementName, elementAtomicNumber, elementAtomicMass,
+					elementMoles);
+			ElementDomainObject edo = em.createElement();
+			edo.persist();
+		}
 		// BOO!!!
 	}
 
@@ -74,6 +90,14 @@ public class AddElementCmd implements CommandInterface {
 
 	public int getAcidID() {
 		return acidID;
+	}
+
+	public double getElementMoles() {
+		return elementMoles;
+	}
+
+	public double getElementMolesOfAcidToDissolve() {
+		return elementMolesOfAcidToDissolve;
 	}
 
 }
