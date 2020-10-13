802bfe19c54ec7f8beb0135aed81e3054f0c8ce4
Marlee Lackey <ml1548@cs.ship.edu>
2020-10-12 20:23:55 -0400

    constructor and all setters (except for elementID) are done; persist still not done

--- PresentationDomain/src/DomainObjects/ElementDomainObject.java
+++ PresentationDomain/src/DomainObjects/ElementDomainObject.java
@@ -8,7 +8,98 @@ package DomainObjects;
  */
 public class ElementDomainObject {
 	
-	public void createConstructor() {
+	/**
+	 * Instance variables
+	 */
+	private ElementDataMapper dataMapper;
+	private int elementID;
+	private String elementName;
+	private int elementAtomicNumber;
+	private double elementAtomicMass;
+	private boolean isMetal;
+	private int acidID;
+	
+	/**
+	 * Constructor
+	 * @param dm the data mapper that created this ElementDomainObject
+	 * @throws Exception throws an exception if one of the setter's throw an exception
+	 */
+	public ElementDomainObject(ElementDataMapper dm) throws Exception {
+		dataMapper = dm;
+		elementID.setElementID(dm.getID());
+        elementName.setElementName(dm.getName());
+        elementAtomicMass.setElementAtomicMass(dm.getAtomicMass());
+        elementAtomicNumber.setElementAtomicNumber(dm.getAtomicNumber());
+        isMetal.setIsMetal(dm.getIsMetal());
+        acidID.setAcidID(dm.getAcidID());
+ 
+	}
+	
+	
+	/**
+	 * Set elementID and check if the passed in elementID is allowed (check business logic)
+	 * @param ID
+	 * @throws Exception throws an exception if business logic is not met
+	 */
+	public void setElementID(int ID) throws Exception {
+		elementID = ID;
+		//TODO: check business logic
 	}
 
+	/**
+	 * Set elementName and check if the passed in elementID is allowed (check business logic)
+	 * @param name
+	 * @throws Exception throws an exception if business logic is not met
+	 */
+	public void setElementName(String name) throws Exception {
+		if(name.contains(" ")) { // if it contains a space, it has 2 words and therefore is illegal
+			throw new Exception("Element name should only be one word with no spaces.");
+		}
+		else {
+			elementName = name;
+		}
+	}
+
+	/**
+	 * Set elementAtomicNumber and check if the passed in elementID is allowed (check business logic)
+	 * @param atomicNumber
+	 * @throws Exception throws an exception if business logic is not met
+	 */
+	public void setElementAtomicNumber(int atomicNumber) throws Exception {
+		if(atomicNumber > elementAtomicMass) {
+			throw new Exception("You can not have an atomic number that is greater than the atomic mass.");
+		}
+		else {
+			elementAtomicNumber = atomicNumber;
+		}
+		
+	}
+
+	/**
+	 * Set elementAtomicMass 
+	 * @param atomicMass
+	 */
+	public void setElementAtomicMass(double atomicMass) {
+		elementAtomicMass = atomicMass;
+	}
+
+	/**
+	 * Set isMetal
+	 * @param isM
+	 */
+	public void setMetal(boolean isM) {
+		isMetal = isM;
+	}
+
+	/**
+	 * Set AcidID 
+	 * @param a_ID
+	 */
+	public void setAcidID(int a_ID) {
+		acidID = a_ID;
+	}
+	
+	public void persist() {
+		
+	}
 }
