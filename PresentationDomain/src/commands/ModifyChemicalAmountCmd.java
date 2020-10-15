9956f930a4223d8f9102ce511f41fc52d7b7e86a
Bev Cardinale <mc6672@cs.ship.edu>
2020-10-14 23:17:44 -0400

    story 3 command done

--- PresentationDomain/src/commands/ModifyChemicalAmountCmd.java
+++ PresentationDomain/src/commands/ModifyChemicalAmountCmd.java
@@ -1,15 +1,30 @@
 package commands;
 
+/**
+ * Command class to modify a chemical's amount in inventory
+ * 
+ * @author Madeline and Adam
+ *
+ */
 public class ModifyChemicalAmountCmd implements CommandInterface {
-	
+
 	private int chemicalID;
 	private double newChemicalAmount;
-	
+
+	/**
+	 * Constructor
+	 * 
+	 * @param id       the ID of the chemical we want to modify
+	 * @param newMoles the new number of moles of the chemical in inventory
+	 */
 	public ModifyChemicalAmountCmd(int id, double newMoles) {
 		chemicalID = id;
 		newChemicalAmount = newMoles;
 	}
-	
+
+	/**
+	 * Execute method to invoke the ModifyChemicalAmountCmd
+	 */
 	@Override
 	public void execute() {
 		ChemicalMapper cm = new ChemicalMapper();
@@ -17,13 +32,13 @@ public class ModifyChemicalAmountCmd implements CommandInterface {
 		cdo.setChemicalAmount(newChemicalAmount);
 		cdo.persist();
 	}
-	
+
 	public int getChemicalID() {
 		return chemicalID;
 	}
-	
+
 	public double getNewChemicalAmount() {
 		return newChemicalAmount;
 	}
-	
+
 }
